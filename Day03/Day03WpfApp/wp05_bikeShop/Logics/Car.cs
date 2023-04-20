82237b70d92abd95cca39d459ac9533725a3e6cf
llsuzn <dltnwls0987522@gmail.com>
2023-04-20 17:27:00 +0900

    4월20일 힘들다ㅜㅜ

---
+++ Day03/Day03WpfApp/wp05_bikeShop/Logics/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace wp05_bikeShop.Logics
+{
+    internal class Car : Notifier       // 값이 바뀌는 것을 인지하여 처리하겠다
+    {
+        private string names;
+        public string Names { 
+            get => names;
+            // 프로퍼티를 변경하는 것
+            set
+            {
+                names = value;
+                OnPropertyChanged("Names");  // Names 프로퍼티가 바뀌었을때 !!
+            }
+        }
+        private double speed;
+        public double Speed { 
+            get => speed; 
+            set
+            {
+                speed = value;
+                OnPropertyChanged(nameof(Speed));
+            }
+        }
+        public Color Colorz { get; set; }
+        public Human Driver { get; set; }
+    }
+}
